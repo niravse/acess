c8df8a97dad3350811d85f5d8c7b029ce9df18cd
niravse <102810226+niravse@users.noreply.github.com>
2024-07-31 08:36:50 +0400

    Update scrape.js

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -9,9 +9,6 @@ exports.handler = async function (event, context) {
     const response = await axios.get(url,{
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Referer': 'https://www.google.com/',
-        'X-Forwarded-For': '102.117.128.114',
       }
     });
     const html = response.data;
