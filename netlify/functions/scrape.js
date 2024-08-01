dce3ede5c2b14dbe0125f0578955728ad58cdfff
niravse <102810226+niravse@users.noreply.github.com>
2024-08-01 13:12:58 +0400

    Update scrape.js

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -5,7 +5,11 @@ exports.handler = async function(event, context) {
 
   try {
     const url = `https://www.google.com/search?vet=10ahUKEwjOrdaa_-KGAxUd1gIHHVVnAZUQ06ACCJsO..i&ei=tltwZqDxGabgxc8Piuy3-AU&opi=89978449&rlz=1C1EJFC&yv=3&rciv=jb&nfpr=0&q=jobs&start=${page}0&asearch=jb_list&cs=1&async=_id:VoQFxe,_pms:hts,_fmt:pc`;
-    const response = await axios.get(url);
+    const response = await axios.get(url,{
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      }
+    });
     
     return {
       statusCode: 200,
