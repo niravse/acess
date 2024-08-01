ea7ad7e248c009a8e0cb22791b4e9baeeda176cc
niravse <102810226+niravse@users.noreply.github.com>
2024-08-01 18:22:13 +0400

    Update scrape.js

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -1,26 +1,32 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 
 exports.handler = async function(event, context) {
   const page = event.queryStringParameters.page || '0'; // Extract page parameter from the query string
 
   try {
     const url = `https://www.google.com/search?vet=10ahUKEwitmdOphOOGAxUy1gIHHV9qA2MQ06ACCOkM..i&ei=FGFwZrqbA7CL7NYPh5W98Ao&opi=89978449&rlz=1C1EJFC&yv=3&rciv=jb&nfpr=0&q=jobs&start=${page}0&asearch=jb_list&cs=1&async=_id:VoQFxe,_pms:hts,_fmt:pc`;
-    const response = await axios.get(url,{
+    const response = await axios.get(url, {
       headers: {
-        'Access-Control-Allow-Origin': '*',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
       }
     });
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ data:response.data })
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Allow requests from any origin
+        'Access-Control-Allow-Headers': 'Content-Type', // Allow specific headers
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS', // Allow specific methods
+      },
+      body: JSON.stringify({ data: response.data })
     };
   } catch (error) {
     return {
       statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Include CORS headers in error response as well
+      },
       body: JSON.stringify({ error: error.message })
     };
   }
 };
-
